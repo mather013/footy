743350520929960008c1069e78a5c1cc27a2f45e
Mark Mather <mathermark@hotmail.com>
2013-12-10 20:54:43 +0000

    Fixed migration error

--- db/migrate/20130618190201_create_competitions.rb
+++ db/migrate/20130618190201_create_competitions.rb
@@ -1,6 +1,6 @@
 class CreateCompetitions < ActiveRecord::Migration
   def change
-    create_table :weeks do |t|
+    create_table :competitions do |t|
       t.integer   :id,          :null => false
       t.string    :week,        :null => false
       t.datetime  :close_date,  :null => false
