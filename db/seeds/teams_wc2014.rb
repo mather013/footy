5a551bdf3a22e335852a53eaf1fec74f616913a5
Mark Mather <mathermark@hotmail.com>
2014-01-16 11:56:36 +0000

    Create teams_wc2014.rb

---
+++ db/seeds/teams_wc2014.rb
@@ -0,0 +1,47 @@
+Team.delete_all
+
+Team.find_or_create_by_id(:id => 1,  :name => 'Algeria')
+Team.find_or_create_by_id(:id => 2,  :name => 'Argentina')
+Team.find_or_create_by_id(:id => 3,  :name => 'Australia')
+Team.find_or_create_by_id(:id => 4,  :name => 'Belgium')
+Team.find_or_create_by_id(:id => 5,  :name => 'Bosnia-Herzegovina')
+Team.find_or_create_by_id(:id => 6,  :name => 'Brazil')
+Team.find_or_create_by_id(:id => 7,  :name => '')
+Team.find_or_create_by_id(:id => 8,  :name => '')
+Team.find_or_create_by_id(:id => 9,  :name => '')
+Team.find_or_create_by_id(:id => 10, :name => '')
+Team.find_or_create_by_id(:id => 11,  :name => '')
+Team.find_or_create_by_id(:id => 12,  :name => '')
+Team.find_or_create_by_id(:id => 12,  :name => '')
+Team.find_or_create_by_id(:id => 12,  :name => '')
+Team.find_or_create_by_id(:id => 12,  :name => '')
+
+	
+
+Brazil
+Cameroon
+Chile	
+Colombia
+Côte d'Ivoire	
+Costa Rica	
+Croatia	
+Ecuador	
+England
+France
+Germany
+Ghana
+Greece
+Honduras
+Iran
+Italy
+Japan	
+Korea Republic
+Mexico
+Netherlands	
+Nigeria	
+Portugal
+Russia	
+Spain
+Switzerland	
+Uruguay
+USA
