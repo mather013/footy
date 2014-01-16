642321faec7c4f34f5f175a60ab230135cd6d3f9
Mark Mather <mathermark@hotmail.com>
2014-01-16 11:59:01 +0000

    Update teams_wc2014.rb

--- db/seeds/teams_wc2014.rb
+++ db/seeds/teams_wc2014.rb
@@ -6,37 +6,35 @@ Team.find_or_create_by_id(:id => 3,  :name => 'Australia')
 Team.find_or_create_by_id(:id => 4,  :name => 'Belgium')
 Team.find_or_create_by_id(:id => 5,  :name => 'Bosnia-Herzegovina')
 Team.find_or_create_by_id(:id => 6,  :name => 'Brazil')
-Team.find_or_create_by_id(:id => 7,  :name => '')
-Team.find_or_create_by_id(:id => 8,  :name => '')
-Team.find_or_create_by_id(:id => 9,  :name => '')
-Team.find_or_create_by_id(:id => 10, :name => '')
-Team.find_or_create_by_id(:id => 11,  :name => '')
-Team.find_or_create_by_id(:id => 12,  :name => '')
-Team.find_or_create_by_id(:id => 12,  :name => '')
-Team.find_or_create_by_id(:id => 12,  :name => '')
-Team.find_or_create_by_id(:id => 12,  :name => '')
+Team.find_or_create_by_id(:id => 7,  :name => 'Cameroon')
+Team.find_or_create_by_id(:id => 8,  :name => 'Chile')
+Team.find_or_create_by_id(:id => 9,  :name => 'Colombia')
+Team.find_or_create_by_id(:id => 10, :name => 'Côte d'Ivoire')
+Team.find_or_create_by_id(:id => 11, :name => 'Costa Rica')
+Team.find_or_create_by_id(:id => 12, :name => 'Croatia')
+Team.find_or_create_by_id(:id => 13, :name => 'Ecuador')
+Team.find_or_create_by_id(:id => 14, :name => 'England')
+Team.find_or_create_by_id(:id => 15, :name => 'France')
+Team.find_or_create_by_id(:id => 16, :name => 'Germany')
+Team.find_or_create_by_id(:id => 17, :name => 'Ghana')
+Team.find_or_create_by_id(:id => 18, :name => 'Greece')
+Team.find_or_create_by_id(:id => 19, :name => 'Honduras')
+Team.find_or_create_by_id(:id => 20, :name => 'Iran')
+Team.find_or_create_by_id(:id => 21, :name => 'Italy')
+Team.find_or_create_by_id(:id => 22, :name => 'Japan')
+Team.find_or_create_by_id(:id => 23, :name => 'Korea Republic')
+Team.find_or_create_by_id(:id => 24, :name => 'Mexico')
+Team.find_or_create_by_id(:id => 24, :name => 'Mexico')
+Team.find_or_create_by_id(:id => 24, :name => 'Mexico')
+Team.find_or_create_by_id(:id => 24, :name => 'Mexico')
+Team.find_or_create_by_id(:id => 24, :name => 'Mexico')
+Team.find_or_create_by_id(:id => 24, :name => 'Mexico')
+
+
 
 	
 
-Brazil
-Cameroon
-Chile	
-Colombia
-Côte d'Ivoire	
-Costa Rica	
-Croatia	
-Ecuador	
-England
-France
-Germany
-Ghana
-Greece
-Honduras
-Iran
-Italy
-Japan	
-Korea Republic
-Mexico
+
 Netherlands	
 Nigeria	
 Portugal
