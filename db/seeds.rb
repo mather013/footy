da40d2c65267cadcd5649d1161120e13395a4916
Mark Mather <mathermark@hotmail.com>
2014-01-17 18:40:09 +0000

    Organising seed files for the different comps

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,3 +3,12 @@ common_seeds.each do |seed_file|
   p "seeding file #{seed_file}"
   load seed_file
 end
+
+competition = "premier_league"
+#competition = "world_cup"
+
+competition_seeds = Dir.glob(File.join(Rails.root, 'db', 'seeds', "#{competition}/*.rb"))
+competition_seeds.each do |seed_file|
+  p "seeding file #{seed_file}"
+  load seed_file
+end
