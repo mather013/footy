8a6afb4c7476a3473994754fbd1039e5483c7456
Mark Mather <mathermark@hotmail.com>
2014-01-29 19:19:41 +0000

    Added uniqueness of fixture/user to bet

--- app/models/bet.rb
+++ app/models/bet.rb
@@ -1,10 +1,8 @@
 class Bet < ActiveRecord::Base
   attr_accessible :id, :fixture_id, :user_id, :value
 
-  scope :bets_for_user_and_fixtures, lambda { |user, fixture_ids| where("user_id = ? and fixture_id in (?)", user.id,fixture_ids) }
+  validates :user_id, :uniqueness => {:scope => :fixture_id}
 
-  def bet_already_placed?
-    true
-  end
+  scope :bets_for_user_and_fixtures, lambda { |user, fixture_ids| where("user_id = ? and fixture_id in (?)", user.id,fixture_ids) }
 
 end
