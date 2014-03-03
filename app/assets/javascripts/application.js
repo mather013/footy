26ad13ec64ad881d0fcf1c3b3471f53c3b098b37
Mark Mather <mathermark@hotmail.com>
2014-03-03 22:58:20 +0000

    Removed twitter bootstrap and created custom css for status labels

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,4 +14,3 @@
 //= require jquery_ujs
 //= require jquery.mobile.min
 //= require_tree .
-//= require bootstrap.min
--- db/seeds/premier_league/weeks.rb
+++ db/seeds/premier_league/weeks.rb
@@ -50,26 +50,26 @@ Week.find_or_create_by_id(:id => 32,
                           :description => 'Premier League 29th Mar',
                           :close_date => DateTime.parse('29-03-2014 12:45:00'))
 
-#Week.find_or_create_by_id(:id => 33,
-#                          :week => '201333',
-#                          :description => 'Premier League 5th Apr',
-#                          :close_date => DateTime.parse('05-04-2014 12:45:00'))
-#
-#Week.find_or_create_by_id(:id => 34,
-#                          :week => '201334',
-#                          :description => 'Premier League 12th Apr',
-#                          :close_date => DateTime.parse('12-04-2014 12:45:00'))
-#
-#Week.find_or_create_by_id(:id => 35,
-#                          :week => '201335',
-#                          :description => 'Premier League 19th Apr',
-#                          :close_date => DateTime.parse('19-04-2014 12:45:00'))
-#
-#Week.find_or_create_by_id(:id => 36,
-#                          :week => '201336',
-#                          :description => 'Premier League 26th Apr',
-#                          :close_date => DateTime.parse('26-04-2014 12:45:00'))
-#
+Week.find_or_create_by_id(:id => 33,
+                          :week => '201333',
+                          :description => 'Premier League 5th Apr',
+                          :close_date => DateTime.parse('05-04-2014 12:45:00'))
+
+Week.find_or_create_by_id(:id => 34,
+                          :week => '201334',
+                          :description => 'Premier League 12th Apr',
+                          :close_date => DateTime.parse('12-04-2014 15:00:00'))
+
+Week.find_or_create_by_id(:id => 35,
+                          :week => '201335',
+                          :description => 'Premier League 19th Apr',
+                          :close_date => DateTime.parse('19-04-2014 12:45:00'))
+
+Week.find_or_create_by_id(:id => 36,
+                          :week => '201336',
+                          :description => 'Premier League 26th Apr',
+                          :close_date => DateTime.parse('26-04-2014 12:45:00'))
+
 #Week.find_or_create_by_id(:id => 37,
 #                          :week => '201337',
 #                          :description => 'Premier League 3rd May',
