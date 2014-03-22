bb3df735d8b498735effc33460b3b40485c0d9cb
silversurfer <mathermark@hotmail.com>
2014-03-22 15:52:02 +0000

    Added env config again! previously lost fixing conflict :s

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,8 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+# Load application specific configurations
+require File.expand_path('../config_loader', __FILE__)
+
 # Initialize the rails application
 Footy::Application.initialize!
