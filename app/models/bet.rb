dd4582a62befe490a38e54088e58b8bc7d56868e
silversurfer <mathermark@hotmail.com>
2014-03-23 18:42:54 +0000

    Minor UI improvements

--- app/models/bet.rb
+++ app/models/bet.rb
@@ -5,4 +5,10 @@ class Bet < ActiveRecord::Base
 
   scope :bets_for_user_and_fixtures, lambda { |user, fixture_ids| where("user_id = ? and fixture_id in (?)", user.id,fixture_ids) }
 
+  def outcome
+    fixture = Fixture.find(self.fixture_id)
+    return "" if fixture.score.nil?
+    return "correct" if self.value == fixture.score.outcome
+    return "wrong"
+  end
 end
