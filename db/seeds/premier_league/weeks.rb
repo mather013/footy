8244822ba344341c94103f8f7634ade15b14b839
silversurfer <mathermark@hotmail.com>
2014-04-06 19:20:50 +0100

    Limits team name to fit screen better. Updated Round close times and Fixture kickoff times

--- db/seeds/premier_league/weeks.rb
+++ db/seeds/premier_league/weeks.rb
@@ -42,44 +42,47 @@ Week.find_or_create_by_id(:id => 29,
 Week.find_or_create_by_id(:id => 30,
                           :week => '201330',
                           :description => 'Premier League 15th Mar',
-                          :close_date => DateTime.parse('15-03-2014 12:45:00'))
+                          :close_date => DateTime.parse('15-03-2014 12:45:00'),
+                          :complete => true)
 
 Week.find_or_create_by_id(:id => 31,
                           :week => '201331',
                           :description => 'Premier League 22nd Mar',
-                          :close_date => DateTime.parse('22-03-2014 12:45:00'))
+                          :close_date => DateTime.parse('22-03-2014 12:45:00'),
+                          :complete => true)
 
 Week.find_or_create_by_id(:id => 32,
                           :week => '201332',
                           :description => 'Premier League 29th Mar',
-                          :close_date => DateTime.parse('29-03-2014 12:45:00'))
+                          :close_date => DateTime.parse('29-03-2014 12:45:00'),
+                          :complete => true)
 
 Week.find_or_create_by_id(:id => 33,
                           :week => '201333',
                           :description => 'Premier League 5th Apr',
-                          :close_date => DateTime.parse('05-04-2014 12:45:00'))
+                          :close_date => DateTime.parse('05-04-2014 11:45:00'))
 
 Week.find_or_create_by_id(:id => 34,
                           :week => '201334',
                           :description => 'Premier League 12th Apr',
-                          :close_date => DateTime.parse('12-04-2014 15:00:00'))
+                          :close_date => DateTime.parse('12-04-2014 14:07:00'))
 
 Week.find_or_create_by_id(:id => 35,
                           :week => '201335',
                           :description => 'Premier League 19th Apr',
-                          :close_date => DateTime.parse('19-04-2014 12:45:00'))
+                          :close_date => DateTime.parse('19-04-2014 11:45:00'))
 
 Week.find_or_create_by_id(:id => 36,
                           :week => '201336',
                           :description => 'Premier League 26th Apr',
-                          :close_date => DateTime.parse('26-04-2014 12:45:00'))
+                          :close_date => DateTime.parse('26-04-2014 11:45:00'))
 
 Week.find_or_create_by_id(:id => 37,
                           :week => '201337',
                           :description => 'Premier League 3rd May',
-                          :close_date => DateTime.parse('03-05-2014 15:00:00'))
+                          :close_date => DateTime.parse('03-05-2014 11:45:00'))
 
 Week.find_or_create_by_id(:id => 38,
                           :week => '201338',
                           :description => 'Premier League 11th May',
-                          :close_date => DateTime.parse('11-05-2014 15:00:00'))
+                          :close_date => DateTime.parse('11-05-2014 14:00:00'))
