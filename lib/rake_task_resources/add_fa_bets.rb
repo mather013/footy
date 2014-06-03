38feb2d2f5c0de224221c4aabb00b5ea67d0a64d
silversurfer <mathermark@hotmail.com>
2014-06-03 20:15:21 +0100

    Updated 5 Alive selections

--- lib/rake_task_resources/add_fa_bets.rb
+++ lib/rake_task_resources/add_fa_bets.rb
@@ -16,9 +16,8 @@ module RakeTaskResources
                 { user_id: User.find_by_username('jonr').id,   player_names: ['R Lukaku',  'Neymar Jr',   'R Van Persie', 'C Ronaldo', 'D Sturridge' ] },
                 { user_id: User.find_by_username('stec').id,   player_names: ['S Aguero',  'K Benzema',   'D Costa',      'R Lukaku',  'A Robben'    ] },
                 { user_id: User.find_by_username('liams').id,  player_names: ['S Aguero',  'L Messi',     'Neymar Jr',    'C Ronaldo', 'Y Toure'     ] },
-                { user_id: User.find_by_username('jonr').id,   player_names: ['R Lukaku',  'Neymar Jr',   'R Van Persie', 'C Ronaldo', 'D Sturridge' ] },
                 { user_id: User.find_by_username('sammyc').id, player_names: ['L Messi',   'Neymar Jr',   'C Ronaldo',    'L Suarez',  'D Sturridge' ] }]
-        #davidm, davem, joshb, mickb, chrisg, peterm, neild, carld, sammyc, marks, steb, genec
+                  #davidm, davem, joshb, mickb, chrisg, peterm, neild, carld, marks, steb, genec
 
         bets.each do |bet|
           bet[:player_names].each do |player_name|
