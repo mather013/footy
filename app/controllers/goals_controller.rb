b832e0cca791aebdff0e64f888f49c3264034784
silversurfer <mathermark@hotmail.com>
2014-06-05 17:48:34 +0100

    Enforcing empty last line convention

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -2,7 +2,6 @@ class GoalsController < ApplicationController
   before_filter :require_login
 
   def index
-    #@goals = Goal.find(:all, :select => 'player_id, count(*) as count, sum(amount) as total', :group => 'player_id', :order => 'total DESC' )
     @goals = Goal.find(:all, :select => 'player_id, count(*) as count', :group => 'player_id', :order => 'count DESC' )
   end
 
