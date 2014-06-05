19cc6dfa2eaa8823a0f98633479fef0efd384482
silversurfer <mathermark@hotmail.com>
2014-06-05 12:54:17 +0100

    Created Goal#opposing_team method - moves logic out of view and into model

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -1,9 +1,13 @@
 class Goal < ActiveRecord::Base
   attr_accessible :id, :player_id, :score_id
 
-  belongs_to :score,  :foreign_key => "score_id"
+  belongs_to :score, :foreign_key => "score_id"
   belongs_to :player, :foreign_key => "player_id"
 
   scope :sorted, order("players.goals DESC")
+
+  def opposing_team
+    self.score.fixture.home_team.id == self.player.team.id ? self.score.fixture.away_team : self.score.fixture.home_team
+  end
 
 end
