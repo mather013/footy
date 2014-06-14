8e9541d135d2a50a508a00b6b02f1d0e47ff2035
silversurfer <mathermark@hotmail.com>
2014-06-14 01:05:30 +0100

    Fix for add_goal for heroku removing quotes

--- lib/rake_task_resources/add_goal.rb
+++ lib/rake_task_resources/add_goal.rb
@@ -2,7 +2,7 @@ module RakeTaskResources
   class AddGoal
     class << self
       def perform args
-        player = Player.find_by_name(args[:player_name])
+        player = Player.find_by_name(args[:player_name].gsub("_"," "))
         score  = Fixture.find_by_name(args[:fixture_name]).score
         goals = args[:amount].to_i
         goals.times do
