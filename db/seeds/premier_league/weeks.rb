f8e948bb34541a459c6417c813f47c1fd4cd5187
silversurfer <mathermark@hotmail.com>
2014-06-22 21:36:23 +0100

    Changed descriptions of rounds

--- db/seeds/premier_league/weeks.rb
+++ db/seeds/premier_league/weeks.rb
@@ -1,77 +1,77 @@
 Week.delete_all
 
 Week.find_or_create_by_id(id: 1,
-                          description: 'Premier League 16th Aug',
+                          description: 'Premier League R01',
                           close_date: DateTime.parse('16-08-2014 12:45:00'))
 
 Week.find_or_create_by_id(id: 2,
-                          description: 'Premier League 23rd Aug',
+                          description: 'Premier League R02',
                           close_date: DateTime.parse('23-08-2014 12:45:00'))
 
 Week.find_or_create_by_id(id: 3,
-                          description: 'Premier League 30th Aug',
+                          description: 'Premier League R03',
                           close_date: DateTime.parse('30-08-2014 12:45:00'))
 
 Week.find_or_create_by_id(id: 4,
-                          description: 'Premier League 13th Sep',
+                          description: 'Premier League R04',
                           close_date: DateTime.parse('13-09-2014 19:45:00'))
 
 Week.find_or_create_by_id(id: 5,
-                          description: 'Premier League 20th Sep',
+                          description: 'Premier League R05',
                           close_date: DateTime.parse('22-09-2014 12:45:00'))
 
 Week.find_or_create_by_id(id: 6,
-                          description: 'Premier League 27th Sep',
+                          description: 'Premier League R06',
                           close_date: DateTime.parse('27-09-2014 12:45:00'))
 
 Week.find_or_create_by_id(id: 7,
-                          description: 'Premier League 4th Oct',
+                          description: 'Premier League R07',
                           close_date: DateTime.parse('04-10-2014 12:45:00'))
 
 Week.find_or_create_by_id(id: 8,
-                          description: 'Premier League 18th Oct',
+                          description: 'Premier League R08',
                           close_date: DateTime.parse('18-10-2014 12:45:00'))
 
 Week.find_or_create_by_id(id: 9,
-                          description: 'Premier League 25th Oct',
+                          description: 'Premier League R09',
                           close_date: DateTime.parse('25-10-2014 12:45:00'))
 
 Week.find_or_create_by_id(id: 10,
-                          description: 'Premier League 1st Nov',
+                          description: 'Premier League R10',
                           close_date: DateTime.parse('01-11-2014 12:45:00'))
 
 Week.find_or_create_by_id(id: 11,
-                          description: 'Premier League 8th Nov',
+                          description: 'Premier League R11',
                           close_date: DateTime.parse('08-11-2014 12:45:00'))
 
 Week.find_or_create_by_id(id: 12,
-                          description: 'Premier League 22nd Nov',
+                          description: 'Premier League R12',
                           close_date: DateTime.parse('22-11-2014 12:45:00'))
 
 Week.find_or_create_by_id(id: 13,
-                          description: 'Premier League 29th Nov',
+                          description: 'Premier League R13',
                           close_date: DateTime.parse('29-11-2014 12:45:00'))
 
 Week.find_or_create_by_id(id: 14,
-                          description: 'Premier League 2nd Dec',
+                          description: 'Premier League R14',
                           close_date: DateTime.parse('02-12-2014 12:45:00'))
 
 Week.find_or_create_by_id(id: 15,
-                          description: 'Premier League 6th Dec',
+                          description: 'Premier League R15',
                           close_date: DateTime.parse('06-12-2014 12:45:00'))
 
 Week.find_or_create_by_id(id: 16,
-                          description: 'Premier League 13th Dec',
+                          description: 'Premier League R16',
                           close_date: DateTime.parse('13-12-2014 12:45:00'))
 
 Week.find_or_create_by_id(id: 17,
-                          description: 'Premier League 20th Dec',
+                          description: 'Premier League R17',
                           close_date: DateTime.parse('20-12-2014 12:45:00'))
 
 Week.find_or_create_by_id(id: 18,
-                          description: 'Premier League 26th Dec',
+                          description: 'Premier League R18',
                           close_date: DateTime.parse('26-12-2014 12:45:00'))
 
 Week.find_or_create_by_id(id: 19,
-                          description: 'Premier League 28th Dec',
+                          description: 'Premier League R19',
                           close_date: DateTime.parse('28-12-2014 12:45:00'))
