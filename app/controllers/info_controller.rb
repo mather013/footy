095fce9d393383215addbcffc874acf7bb32150f
silversurfer <mathermark@hotmail.com>
2014-08-22 13:39:39 +0100

    Info page displays users ordered by username. Also updated some fixtures

--- app/controllers/info_controller.rb
+++ app/controllers/info_controller.rb
@@ -2,7 +2,7 @@ class InfoController < ApplicationController
   before_filter :require_login
 
   def index
-    @users = User.all
+    @users = User.order('username')
     @user = current_user
   end
 
