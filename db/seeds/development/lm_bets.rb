530f666b658b612aef95fd2000bd830b02a6e926
silversurfer <mathermark@hotmail.com>
2014-09-02 10:52:58 +0100

    Updating epl development data for testing

--- db/seeds/development/lm_bets.rb
+++ db/seeds/development/lm_bets.rb
@@ -1,22 +1,20 @@
 LmBet.delete_all
-#LmBet.create(lm_round_id: 1, user_id: 1, team_id: 1)
-#LmBet.create(lm_round_id: 2, user_id: 1, team_id: 9)
-#LmBet.create(lm_round_id: 3, user_id: 1, team_id: 2)
+LmBet.create(lm_round_id: 1, user_id: 1, team_id: 1) #ARS
+LmBet.create(lm_round_id: 2, user_id: 1, team_id: 9) #LIV
+LmBet.create(lm_round_id: 3, user_id: 1, team_id: 2) #AST
 #LmBet.create(lm_round_id: 4, user_id: 1, team_id: 10) #3
 #LmBet.create(lm_round_id: 5, user_id: 1, team_id: 12)
 
-#LmBet.create(lm_round_id: 1, user_id: 2, team_id: 8)
-#LmBet.create(lm_round_id: 2, user_id: 2, team_id: 12)
+LmBet.create(lm_round_id: 1, user_id: 2, team_id: 8) #LEI
+LmBet.create(lm_round_id: 2, user_id: 2, team_id: 12) #NEW
 #LmBet.create(lm_round_id: 3, user_id: 2, team_id: 1)
 
-#LmBet.create(lm_round_id: 1, user_id: 3, team_id: 1)
+LmBet.create(lm_round_id: 1, user_id: 3, team_id: 1) #ARS
 #LmBet.create(lm_round_id: 2, user_id: 3, team_id: 9)
 #LmBet.create(lm_round_id: 3, user_id: 3, team_id: 4)
 #LmBet.create(lm_round_id: 4, user_id: 3, team_id: 10)
 
 
-LmBet.create(lm_round_id: 1, user_id: 1, team_id: 1)
-
-LmBet.create(lm_round_id: 1, user_id: 2, team_id: 5)
-
-LmBet.create(lm_round_id: 1, user_id: 3, team_id: 3)
+#LmBet.create(lm_round_id: 1, user_id: 1, team_id: 1)
+#LmBet.create(lm_round_id: 1, user_id: 2, team_id: 5)
+#LmBet.create(lm_round_id: 1, user_id: 3, team_id: 3)
