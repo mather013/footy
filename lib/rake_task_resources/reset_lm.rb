8eada3b834b5fb16c2b2308cbd02204279a5aa9f
silversurfer <mathermark@hotmail.com>
2014-09-30 09:36:54 +0100

    lms reset rake task now also resets lms points

--- lib/rake_task_resources/reset_lm.rb
+++ lib/rake_task_resources/reset_lm.rb
@@ -2,6 +2,7 @@ module RakeTaskResources
   class ResetLm
     class << self
       def perform week_id
+        LmPoint.delete_all
         LmBet.delete_all
         LmRound.delete_all
         LmRound.find_or_create_by_id(id: 1, week_id: week_id)
