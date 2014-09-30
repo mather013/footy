9c9955d17a6786367a6c813751bdedd196b2b2df
silversurfer <mathermark@hotmail.com>
2014-09-30 09:34:47 +0100

    Updated development data seeds

--- db/seeds/development/lm_bets.rb
+++ db/seeds/development/lm_bets.rb
@@ -1,12 +1,12 @@
 LmBet.delete_all
-LmBet.create(lm_round_id: 1, user_id: 1, team_id: 1) #ARS
-LmBet.create(lm_round_id: 2, user_id: 1, team_id: 9) #LIV
-LmBet.create(lm_round_id: 3, user_id: 1, team_id: 2) #AST
+LmBet.create(lm_round_id: 1, user_id: 1, team_id: 12) #NEW
+#LmBet.create(lm_round_id: 2, user_id: 1, team_id: 9) #LIV
+#LmBet.create(lm_round_id: 3, user_id: 1, team_id: 2) #AST
 #LmBet.create(lm_round_id: 4, user_id: 1, team_id: 10) #3
 #LmBet.create(lm_round_id: 5, user_id: 1, team_id: 12)
 
 LmBet.create(lm_round_id: 1, user_id: 2, team_id: 8) #LEI
-LmBet.create(lm_round_id: 2, user_id: 2, team_id: 12) #NEW
+#LmBet.create(lm_round_id: 2, user_id: 2, team_id: 12) #NEW
 #LmBet.create(lm_round_id: 3, user_id: 2, team_id: 1)
 
 LmBet.create(lm_round_id: 1, user_id: 3, team_id: 1) #ARS
--- db/seeds/development/weeks.rb
+++ db/seeds/development/weeks.rb
@@ -1,8 +1,8 @@
-Week.delete_all
-
-Week.find_or_create_by_id(id: 1,  description: 'Week 01', close_date: DateTime.parse('16-08-2014 11:45:00'), complete: true)
-Week.find_or_create_by_id(id: 2,  description: 'Week 02', close_date: DateTime.parse('23-08-2014 11:45:00'))
-Week.find_or_create_by_id(id: 3,  description: 'Week 03', close_date: DateTime.parse('30-08-2014 11:45:00'))
-Week.find_or_create_by_id(id: 4,  description: 'Week 04', close_date: DateTime.parse('13-09-2014 11:45:00'))
-Week.find_or_create_by_id(id: 5,  description: 'Week 05', close_date: DateTime.parse('24-09-2014 11:45:00'))
-Week.find_or_create_by_id(id: 6,  description: 'Week 06', close_date: DateTime.parse('27-09-2014 11:45:00'))
+#Week.delete_all
+#
+#Week.find_or_create_by_id(id: 1,  description: 'Week 01', close_date: DateTime.parse('16-08-2014 11:45:00'), complete: true)
+#Week.find_or_create_by_id(id: 2,  description: 'Week 02', close_date: DateTime.parse('23-08-2014 11:45:00'))
+#Week.find_or_create_by_id(id: 3,  description: 'Week 03', close_date: DateTime.parse('30-08-2014 11:45:00'))
+#Week.find_or_create_by_id(id: 4,  description: 'Week 04', close_date: DateTime.parse('13-09-2014 11:45:00'))
+#Week.find_or_create_by_id(id: 5,  description: 'Week 05', close_date: DateTime.parse('24-09-2014 11:45:00'))
+#Week.find_or_create_by_id(id: 6,  description: 'Week 06', close_date: DateTime.parse('27-09-2014 11:45:00'))
