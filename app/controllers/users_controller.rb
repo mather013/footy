e32fb961cfe16cddc85de4adc17527733b7cbecf
silversurfer <mathermark@hotmail.com>
2014-10-21 10:04:34 +0100

    Allow users to see other user's predictions but only after kickoff of week

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,12 @@
 class UsersController < ApplicationController
   before_filter :require_login
 
-  def show
+  def index
     @week = Week.find(params[:week_id])
     redirect_to root_url if @week.status == 'Open'
 
     @fixtures = @week.fixtures
-    @view_user = User.find(params[:id])
+    @view_user = User.find(params[:user_id])
   end
 
 end
