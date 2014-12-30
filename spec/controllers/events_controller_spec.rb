e27c01a7d5feee2d1f45bf05cf685198d20e077e
silversurfer <mathermark@hotmail.com>
2014-12-30 15:50:07 +0000

    Imporving events controller tests for ordering by minute asc

--- spec/controllers/events_controller_spec.rb
+++ spec/controllers/events_controller_spec.rb
@@ -7,12 +7,14 @@ describe EventsController do
   end
 
   describe 'index' do
+    let(:fixture) { Fixture.create(week_id: week.id, name: 'ARG-BRA', home_team_id: home_team.id, away_team_id: away_team.id, kickoff: 1.days.ago) }
     let(:home_team) { Team.create(name: 'Argentina', abbreviation: 'arg') }
     let(:away_team) { Team.create(name: 'Brazil', abbreviation: 'bra') }
-    let(:fixture) { Fixture.create(week_id: week.id, name: 'ARG-BRA', home_team_id: home_team.id, away_team_id: away_team.id, kickoff: 1.days.ago) }
     let(:week) { Week.create(description: 'Week 01', close_date: 1.days.ago) }
-    let!(:events) { [Event.create(fixture_id: fixture.id, event_type: 'goal', team: 'visitorteam', player_name: 'Neymar', minute: 10),
-                     Event.create(fixture_id: fixture.id, event_type: 'goal', team: 'localteam', player_name: 'Aguero', minute: 20)] }
+
+    let!(:event_one) { Event.create(fixture_id: fixture.id, event_type: 'goal', team: 'visitorteam', player_name: 'Neymar', minute: 20) }
+    let!(:event_two) { Event.create(fixture_id: fixture.id, event_type: 'goal', team: 'localteam', player_name: 'Aguero', minute: 10) }
+    let(:events) { [event_one,event_two] }
 
     it 'should be successful' do
       get :index, week_id: week.id, fixture_id: fixture.id
@@ -24,8 +26,11 @@ describe EventsController do
       get :index, week_id: week.id, fixture_id: fixture.id
     end
 
-    xit 'should fetch events for fixture' do
-
+    it 'should fetch events for fixture' do
+      Fixture.stub(:find).and_return(fixture)
+      fixture.should_receive(:events).and_return(events)
+      events.should_receive(:order).with(:minute).and_return([event_two, event_one])
+      get :index, week_id: week.id, fixture_id: fixture.id
     end
 
   end
