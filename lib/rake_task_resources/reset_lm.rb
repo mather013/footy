03d0f9df30dcfd6ba1c2e97278dc8541a8845c5e
silversurfer <mathermark@hotmail.com>
2014-12-30 15:47:51 +0000

    Last Man Standing rake reset now updates all users status to 'not paid' for this game (removes game from user)

--- lib/rake_task_resources/reset_lm.rb
+++ lib/rake_task_resources/reset_lm.rb
@@ -6,7 +6,18 @@ module RakeTaskResources
         LmBet.delete_all
         LmRound.delete_all
         LmRound.find_or_create_by_id(id: 1, week_id: week_id)
+        remove_lm_game_from_all_users
       end
+
+      private
+
+      def remove_lm_game_from_all_users
+        lm_game= Game.find(4)
+        User.all.each do |user|
+          user.games.delete(lm_game)
+        end
+      end
+
     end
   end
 end
