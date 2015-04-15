726f133b2d19e6c7afacbb01f024a0a12f910333
silversurfer <mathermark@hotmail.com>
2015-04-15 19:16:54 +0100

    Replacing name with forename and surname in Player model - missed schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20150109204802) do
+ActiveRecord::Schema.define(:version => 20150330114051) do
 
   create_table "bets", :force => true do |t|
     t.integer  "fixture_id", :null => false
@@ -107,10 +107,11 @@ ActiveRecord::Schema.define(:version => 20150109204802) do
   end
 
   create_table "players", :force => true do |t|
-    t.string   "name",       :null => false
     t.integer  "team_id",    :null => false
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.string   "surname"
+    t.string   "forename"
   end
 
   create_table "points", :force => true do |t|
