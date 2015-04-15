9bd2094b6bc8a199ba98bbfcaff410cb3ba4eda2
silversurfer <mathermark@hotmail.com>
2015-04-15 20:03:13 +0100

    Copa America 2015 initial commit

---
+++ db/seeds/copa_cup/players.rb
@@ -0,0 +1,79 @@
+Player.delete_all
+
+#argentina
+Player.create(id: 1,   team_id: 1,  forename: 'S',    surname: 'Aguero')
+Player.create(id: 2,   team_id: 1,  forename: 'G',    surname: 'Higuain')
+Player.create(id: 3,   team_id: 1,  forename: 'A di', surname: 'Maria')
+Player.create(id: 4,   team_id: 1,  forename: 'L',    surname: 'Messi')
+Player.create(id: 5,   team_id: 1,  forename: 'C',    surname: 'Tevez')
+
+#bolivia
+Player.create(id: 21,  team_id: 2,  forename: 'J',    surname: 'Arce')
+Player.create(id: 22,  team_id: 2,  forename: 'C',    surname: 'Saucedo')
+Player.create(id: 23,  team_id: 2,  forename: 'M',    surname: 'Moreno')
+
+#brazil
+Player.create(id: 41,  team_id: 3,  forename: nil,    surname: 'Neymar Jr')
+Player.create(id: 42,  team_id: 3,  forename: nil,    surname: 'Oscar')
+Player.create(id: 43,  team_id: 3,  forename: 'L',    surname: 'Gustavo')
+Player.create(id: 44,  team_id: 3,  forename: 'B de', surname: 'Oliveira')
+Player.create(id: 45,  team_id: 3,  forename: 'L',    surname: 'Adriano')
+Player.create(id: 46,  team_id: 3,  forename: nil,    surname: 'Fernandinho')
+Player.create(id: 47,  team_id: 3,  forename: 'P',    surname: 'Coutinho')
+Player.create(id: 48,  team_id: 3,  forename: nil,    surname: 'Robinho')
+Player.create(id: 49,  team_id: 3,  forename: nil,    surname: 'Willian')
+
+#chile
+Player.create(id: 61,  team_id: 4,  forename: 'A', surname: 'Sanchez')
+Player.create(id: 62,  team_id: 4,  forename: 'P', surname: 'Hernandez')
+Player.create(id: 63,  team_id: 4,  forename: 'A', surname: 'Vidal')
+Player.create(id: 64,  team_id: 4,  forename: 'E', surname: 'Vargas')
+Player.create(id: 65,  team_id: 4,  forename: 'M', surname: 'Fernandez')
+Player.create(id: 66,  team_id: 4,  forename: 'D', surname: 'Pizarro')
+
+#colombia
+Player.create(id: 81,  team_id: 5,  forename: 'C', surname: 'Bacca')
+Player.create(id: 82,  team_id: 5,  forename: 'T', surname: 'Gutierrez')
+Player.create(id: 83,  team_id: 5,  forename: 'J', surname: 'Martinez')
+Player.create(id: 84,  team_id: 5,  forename: 'J', surname: 'Rodriguez')
+Player.create(id: 85,  team_id: 5,  forename: 'R', surname: 'Falcao')
+Player.create(id: 86,  team_id: 5,  forename: 'A', surname: 'Ramos')
+Player.create(id: 87,  team_id: 5,  forename: 'W', surname: 'Renteria')
+
+#ecuador
+Player.create(id: 101, team_id: 6,  forename: 'A', surname: 'Valencia')
+Player.create(id: 102, team_id: 6,  forename: 'F', surname: 'Caicedo')
+Player.create(id: 103, team_id: 6,  forename: 'E', surname: 'Valencia')
+
+#jamaica
+Player.create(id: 121, team_id: 7,  forename: 'M', surname: 'Unknown')
+
+
+#mexico
+Player.create(id: 141, team_id: 8,  forename: 'J',     surname: 'Hernandez')
+Player.create(id: 142, team_id: 8,  forename: 'G dos', surname: 'Santos')
+Player.create(id: 143, team_id: 8,  forename: 'M',     surname: 'Layun')
+Player.create(id: 144, team_id: 8,  forename: 'C',     surname: 'Vela')
+Player.create(id: 145, team_id: 8,  forename: 'A',     surname: 'Guardado')
+
+#paraguay
+Player.create(id: 161, team_id: 10,  forename: 'S la', surname: 'Cruz')
+Player.create(id: 162, team_id: 10,  forename: 'E', surname: 'Sambenito')
+Player.create(id: 163, team_id: 10,  forename: 'A', surname: 'Santanabria')
+
+
+#peru
+Player.create(id: 181, team_id: 10,  forename: 'I', surname: 'Avila')
+Player.create(id: 182, team_id: 10,  forename: 'P', surname: 'Hurtado')
+Player.create(id: 183, team_id: 10,  forename: 'A', surname: 'Carrillo')
+Player.create(id: 184, team_id: 10,  forename: 'C', surname: 'Cueva')
+
+
+#uruguay
+Player.create(id: 201, team_id: 11, forename: 'E', surname: 'Cavani')
+Player.create(id: 202, team_id: 11, forename: 'L', surname: 'Suarez')
+
+
+#venezuela
+Player.create(id: 221, team_id: 12, forename: 'M', surname: 'Romeron')
+Player.create(id: 222, team_id: 12, forename: 'S', surname: 'Romeron')
---
+++ db/seeds/copa_cup/teams.rb
@@ -0,0 +1,16 @@
+Team.delete_all
+
+Team.find_or_create_by_id(id: 1,  abbreviation: 'arg', name: 'Argentina', external_id: nil)
+Team.find_or_create_by_id(id: 2,  abbreviation: 'bol', name: 'Bolivia',   external_id: nil)
+Team.find_or_create_by_id(id: 3,  abbreviation: 'bra', name: 'Brazil',    external_id: nil)
+Team.find_or_create_by_id(id: 4,  abbreviation: 'chi', name: 'Chile',     external_id: nil)
+Team.find_or_create_by_id(id: 5,  abbreviation: 'col', name: 'Colombia',  external_id: nil)
+Team.find_or_create_by_id(id: 6,  abbreviation: 'ecu', name: 'Ecuador',   external_id: nil)
+Team.find_or_create_by_id(id: 7,  abbreviation: 'jam', name: 'Jamaica',   external_id: nil)
+Team.find_or_create_by_id(id: 8,  abbreviation: 'mex', name: 'Mexico',    external_id: nil)
+Team.find_or_create_by_id(id: 9,  abbreviation: 'par', name: 'Paraguay',  external_id: nil)
+Team.find_or_create_by_id(id: 10, abbreviation: 'per', name: 'Peru',      external_id: nil)
+Team.find_or_create_by_id(id: 11, abbreviation: 'uru', name: 'Uruguay',   external_id: nil)
+Team.find_or_create_by_id(id: 12, abbreviation: 'ven', name: 'Venezuela', external_id: nil)
+
+Team.find_or_create_by_id(id: 13, abbreviation: 'tba', name: 'TBA')
