06e4289d762a88717bc4c53b6648dcefdeab62ba
silversurfer <mathermark@hotmail.com>
2015-07-15 18:45:31 +0100

    Task to reset data for new comp now includes weeks and fixtures

--- lib/rake_task_resources/reset_data.rb
+++ lib/rake_task_resources/reset_data.rb
@@ -2,20 +2,23 @@ module RakeTaskResources
   class ResetData
     class << self
       def perform
-        Point.delete_all
         Bet.delete_all
-        FaPoint.delete_all
-        FaBet.delete_all
-        LmPoint.delete_all
-        LmBet.delete_all
-        Goal.delete_all
-        Score.delete_all
-        LmRound.delete_all
-        Position.delete_all
-        Standing.delete_all
-        Fixture.delete_all
         Communication.delete_all
         Event.delete_all
+        FaBet.delete_all
+        FaPoint.delete_all
+        Fixture.delete_all
+        Goal.delete_all
+        LmBet.delete_all
+        LmPoint.delete_all
+        LmRound.delete_all
+        Player.delete_all
+        Point.delete_all
+        Position.delete_all
+        Score.delete_all
+        Standing.delete_all
+        Team.delete_all
+        Week.delete_all
       end
     end
   end
