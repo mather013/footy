f87cdd16f470b46904a473bbcbfc632c9deb5d76
silversurfer <mathermark@hotmail.com>
2015-07-29 18:55:38 +0100

    UI shows weekly HDA winner and LMS jackpot

--- app/models/point.rb
+++ app/models/point.rb
@@ -1,6 +1,8 @@
 class Point < ActiveRecord::Base
   attr_accessible :id, :week_id, :user_id, :value
 
+  belongs_to :user
+
   scope :sorted, order('value DESC')
   scope :point_for_user_and_week, lambda { |user, week| where('user_id = ? and week_id = ?',user.id,week.id) }
 
