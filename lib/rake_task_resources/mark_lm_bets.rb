78816e4be32429a85551a9720f755c2fd933f360
silversurfer <mathermark@hotmail.com>
2015-08-19 19:29:34 +0100

    Adds new LMS round automatically when syncing scores and marking

--- lib/rake_task_resources/mark_lm_bets.rb
+++ lib/rake_task_resources/mark_lm_bets.rb
@@ -10,20 +10,23 @@ module RakeTaskResources
             award_points user, points
           end
         end
+        add_new_round if LmRound.last.week.id < Week.current.id
       end
 
       private
 
       def mark_last_man lm_bets
         points = 0
-        lm_bets.each do |lm_bet|
-          points +=1 unless lm_bet.result == 'L' || lm_bet.result == 'D'
-        end
+        lm_bets.each { |lm_bet| points +=1 unless lm_bet.result == 'L' || lm_bet.result == 'D' }
         points
       end
 
       def award_points user, points
         user.lm_point.present? ? user.lm_point.update_attributes(value: points) : LmPoint.create(user_id: user.id, value: points)
+      end
+
+      def add_new_round
+        RakeTaskResources::AddLmRound.perform
       end
 
     end
