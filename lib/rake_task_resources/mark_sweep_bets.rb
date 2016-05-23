4a8f640848bcde89abd5971ddded711d22c5c717
silversurfer <mathermark@hotmail.com>
2016-05-23 22:23:17 +0100

    Added rake task to mark sweepstake bets

---
+++ lib/rake_task_resources/mark_sweep_bets.rb
@@ -0,0 +1,41 @@
+module RakeTaskResources
+  class MarkGbBets
+    class << self
+
+      MAX_VALUE = 11
+
+      def perform
+        puts "marking goal buster" unless Rails.env.test?
+
+        hash = {}
+        (1..Team.count).to_a.each { |team_id| hash[team_id] = 0 }
+
+        fixtures.each do |fixture|
+          if fixture.score.present?
+            hash[fixture.home_team_id] += fixture.score.home unless (hash[fixture.home_team_id] + fixture.score.home) > MAX_VALUE
+            hash[fixture.away_team_id] += fixture.score.away unless (hash[fixture.away_team_id] + fixture.score.away) > MAX_VALUE
+          end
+        end
+
+        hash.each do |team_id, goals|
+          event = goals > 0 ? last_goal_event(Team.find(team_id)) : nil
+          minute = event.present? ? event.minute : nil
+          GbPoint.find_by_team_id(team_id).update_attributes(value: goals, minute: minute)
+        end
+      end
+
+      private
+
+      def fixtures
+        Fixture.where('kickoff >= ?',GbRound.first.week.close_date).order('kickoff asc')
+      end
+
+      def last_goal_event team
+        #team.events.where(event_type:['goal','owngoal']).order('id desc').first
+        #team.events.where('event_type in (?) and created_at >= ?', ['goal','owngoal'],weeks.first.close_date).order('id desc').first
+        nil
+      end
+
+    end
+  end
+end
