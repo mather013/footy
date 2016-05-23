c2bf7af5f5f7e42c6e7880d52c3b5e11f3a61ded
silversurfer <mathermark@hotmail.com>
2016-05-23 22:26:17 +0100

    Improved rake task to mark sweepstake bets

--- lib/rake_task_resources/mark_sweep_bets.rb
+++ lib/rake_task_resources/mark_sweep_bets.rb
@@ -1,41 +1,29 @@
 module RakeTaskResources
-  class MarkGbBets
+  class MarkSweepBets
     class << self
 
-      MAX_VALUE = 11
-
       def perform
-        puts "marking goal buster" unless Rails.env.test?
-
-        hash = {}
-        (1..Team.count).to_a.each { |team_id| hash[team_id] = 0 }
+        puts "marking sweepstake" unless Rails.env.test?
 
         fixtures.each do |fixture|
           if fixture.score.present?
-            hash[fixture.home_team_id] += fixture.score.home unless (hash[fixture.home_team_id] + fixture.score.home) > MAX_VALUE
-            hash[fixture.away_team_id] += fixture.score.away unless (hash[fixture.away_team_id] + fixture.score.away) > MAX_VALUE
+            teams = fixture.teams
+            teams.delete(fixture.winning_team)
+            losing_team = teams.first
+            SweepPoint.find_by_team_id(losing_team.id).update_attributes(value: fixture.week_id)
           end
-        end
-
-        hash.each do |team_id, goals|
-          event = goals > 0 ? last_goal_event(Team.find(team_id)) : nil
-          minute = event.present? ? event.minute : nil
-          GbPoint.find_by_team_id(team_id).update_attributes(value: goals, minute: minute)
         end
       end
 
       private
 
+      def knockout_rounds
+        ENVIRONMENT_CONFIG["#{ENVIRONMENT_CONFIG['competition']}"]['knockout_rounds']
+      end
+
       def fixtures
-        Fixture.where('kickoff >= ?',GbRound.first.week.close_date).order('kickoff asc')
+        Week.where('close_date < now() and id in (?)', knockout_rounds).map(&:fixtures).flatten
       end
-
-      def last_goal_event team
-        #team.events.where(event_type:['goal','owngoal']).order('id desc').first
-        #team.events.where('event_type in (?) and created_at >= ?', ['goal','owngoal'],weeks.first.close_date).order('id desc').first
-        nil
-      end
-
     end
   end
 end
--- spec/rake_task_resources/mark_sweep_bets_spec.rb
+++ spec/rake_task_resources/mark_sweep_bets_spec.rb
@@ -1,132 +1,48 @@
 require 'spec_helper'
 
 module RakeTaskResources
-  describe MarkGbBets do
+  describe MarkSweepBets do
 
     describe ".perform" do
-      let(:team_hash) { { arg: 1, bra: 2, col: 3, den: 4, eng: 5, fra: 6 } }
+      let(:team_hash) { {arg: 1, bra: 2, col: 3, den: 4, eng: 5, fra: 6, ger: 7, hol: 8} }
 
-      let(:week_three_ko) { 1.days.ago }
-      let!(:week_three) { Week.create(id: 3, close_date: week_three_ko, description: 'Week 03') }
-      let!(:fixture_7) { week_three.fixtures.create(id:7, kickoff: week_three_ko, home_team_id: 1, away_team_id: 4) }
-      let!(:fixture_7_score) { fixture_7.create_score(home:0, away:0) }
+      let(:week_5_ko) { 21.days.ago }
+      let!(:week_5) { Week.create(id: 5, close_date: week_5_ko, description: 'Quarter-Final') }
+      let!(:fixture_1) { week_5.fixtures.create(id: 1, kickoff: week_5_ko, home_team_id: 1, away_team_id: 5) }
+      let!(:fixture_2) { week_5.fixtures.create(id: 2, kickoff: week_5_ko, home_team_id: 2, away_team_id: 6) }
+      let!(:fixture_3) { week_5.fixtures.create(id: 3, kickoff: week_5_ko, home_team_id: 3, away_team_id: 7) }
+      let!(:fixture_4) { week_5.fixtures.create(id: 4, kickoff: week_5_ko, home_team_id: 4, away_team_id: 8) }
+      let!(:fixture_1_score) { fixture_1.create_score(home: 1, away: 0) }
+      let!(:fixture_2_score) { fixture_2.create_score(home: 2, away: 1) }
+      let!(:fixture_3_score) { fixture_3.create_score(home: 3, away: 1) }
+      let!(:fixture_4_score) { fixture_4.create_score(home: 1, away: 0) }
 
-      let!(:fixture_8) { week_three.fixtures.create(id:8, kickoff: week_three_ko, home_team_id: 2, away_team_id: 5) }
-      let!(:fixture_8_score) { fixture_8.create_score(home:1, away:0) }
-      let!(:fixture_8_events) { [fixture_8.events.create(event_type: 'goal', minute: 18, team_id: 2)] }
+      let(:week_6_ko) { 14.days.ago }
+      let!(:week_6) { Week.create(id: 6, close_date: week_6_ko, description: 'Semi-Final') }
+      let!(:fixture_5) { week_6.fixtures.create(id: 5, kickoff: week_6_ko, home_team_id: 1, away_team_id: 2) }
+      let!(:fixture_6) { week_6.fixtures.create(id: 6, kickoff: week_6_ko, home_team_id: 3, away_team_id: 4) }
+      let!(:fixture_5_score) { fixture_5.create_score(home: 0, away: 2) }
+      let!(:fixture_6_score) { fixture_6.create_score(home: 5, away: 1) }
 
-      let!(:fixture_9) { week_three.fixtures.create(id:9, kickoff: week_three_ko, home_team_id: 3, away_team_id: 6) }
-      let!(:fixture_9_score) { fixture_9.create_score(home:6, away:0) }
-      let!(:fixture_9_events) { [fixture_9.events.create(event_type: 'goal', minute: 14, team_id: 3)] }
-      let!(:fixture_9_events) { [fixture_9.events.create(event_type: 'goal', minute: 15, team_id: 3)] }
-      let!(:fixture_9_events) { [fixture_9.events.create(event_type: 'goal', minute: 16, team_id: 3)] }
-      let!(:fixture_9_events) { [fixture_9.events.create(event_type: 'goal', minute: 17, team_id: 3)] }
-      let!(:fixture_9_events) { [fixture_9.events.create(event_type: 'goal', minute: 18, team_id: 3)] }
-      let!(:fixture_9_events) { [fixture_9.events.create(event_type: 'goal', minute: 19, team_id: 3)] }
-
-      let(:week_one_ko) { 14.days.ago }
-      let!(:week_one) { Week.create(id: 1, close_date: week_one_ko, description: 'Week 01') }
-      let!(:fixture_1) { week_one.fixtures.create(id:1, kickoff: week_one_ko, home_team_id: 1, away_team_id: 2) }
-      let!(:fixture_1_score) { fixture_1.create_score(home:0, away:2) }
-      let!(:fixture_1_events) { [fixture_1.events.create(event_type: 'goal', minute: 21, team_id: 2),
-                                 fixture_1.events.create(event_type: 'owngoal', minute: 22, team_id: 2)] }
-
-      let!(:fixture_2) { week_one.fixtures.create(id:2, kickoff: week_one_ko, home_team_id: 3, away_team_id: 4) }
-      let!(:fixture_2_score) { fixture_2.create_score(home:5, away:1) }
-      let!(:fixture_2_events) { [fixture_2.events.create(event_type: 'goal', minute: 31, team_id: 3),
-                                 fixture_2.events.create(event_type: 'goal', minute: 32, team_id: 3),
-                                 fixture_2.events.create(event_type: 'goal', minute: 33, team_id: 3),
-                                 fixture_2.events.create(event_type: 'goal', minute: 34, team_id: 3),
-                                 fixture_2.events.create(event_type: 'goal', minute: 35, team_id: 3),
-                                 fixture_2.events.create(event_type: 'goal', minute: 41, team_id: 4)] }
-
-      let!(:fixture_3) { week_one.fixtures.create(id:3, kickoff: week_three_ko, home_team_id: 5, away_team_id: 6) }
-      let!(:fixture_3_score) { fixture_3.create_score(home:2, away:8) }
-      let!(:fixture_3_events) { [fixture_3.events.create(event_type: 'goal', minute: 51, team_id: 5),
-                                 fixture_3.events.create(event_type: 'goal', minute: 52, team_id: 5),
-                                 fixture_3.events.create(event_type: 'goal', minute: 61, team_id: 6),
-                                 fixture_3.events.create(event_type: 'goal', minute: 62, team_id: 6),
-                                 fixture_3.events.create(event_type: 'goal', minute: 63, team_id: 6),
-                                 fixture_3.events.create(event_type: 'goal', minute: 64, team_id: 6),
-                                 fixture_3.events.create(event_type: 'goal', minute: 65, team_id: 6),
-                                 fixture_3.events.create(event_type: 'goal', minute: 66, team_id: 6),
-                                 fixture_3.events.create(event_type: 'goal', minute: 67, team_id: 6),
-                                 fixture_3.events.create(event_type: 'goal', minute: 68, team_id: 6)] }
-
-      let(:week_two_ko) { 7.days.ago }
-      let!(:week_two) { Week.create(id: 2, close_date: week_two_ko, description: 'Week 02') }
-      let!(:fixture_4) { week_two.fixtures.create(id:4, kickoff: week_two_ko, home_team_id: 2, away_team_id: 1) }
-      let!(:fixture_4_score) { fixture_4.create_score(home:4, away:0) }
-      let!(:fixture_4_events) { [fixture_4.events.create(event_type: 'goal', minute: 21, team_id: 2),
-                                 fixture_4.events.create(event_type: 'goal', minute: 22, team_id: 2),
-                                 fixture_4.events.create(event_type: 'goal', minute: 23, team_id: 2),
-                                 fixture_4.events.create(event_type: 'owngoal', minute: 24, team_id: 2)] }
-
-      let!(:fixture_5) { week_two.fixtures.create(id:5, kickoff: week_two_ko, home_team_id: 4, away_team_id: 3) }
-      let!(:fixture_5_score) { fixture_5.create_score(home:0, away:4) }
-      let!(:fixture_5_events) { [fixture_5.events.create(event_type: 'goal', minute: 31, team_id: 3),
-                                 fixture_5.events.create(event_type: 'goal', minute: 32, team_id: 3),
-                                 fixture_5.events.create(event_type: 'goal', minute: 33, team_id: 3),
-                                 fixture_5.events.create(event_type: 'owngoal', minute: 34, team_id: 3)] }
-
-      let!(:fixture_6) { week_two.fixtures.create(id:6, kickoff: week_two_ko, home_team_id: 6, away_team_id: 5) }
-      let!(:fixture_6_score) { fixture_6.create_score(home:3, away:1) }
-      let!(:fixture_6_events) { [fixture_6.events.create(event_type: 'goal', minute: 61, team_id: 6),
-                                 fixture_6.events.create(event_type: 'goal', minute: 62, team_id: 6),
-                                 fixture_6.events.create(event_type: 'goal', minute: 63, team_id: 6),
-                                 fixture_6.events.create(event_type: 'owngoal', minute: 51, team_id: 5)] }
+      let(:week_7_ko) { 1.day.ago }
+      let!(:week_7) { Week.create(id: 7, close_date: week_7_ko, description: 'Final') }
+      let!(:fixture_7) { week_7.fixtures.create(id: 7, kickoff: week_7_ko, home_team_id: 2, away_team_id: 3) }
+      let!(:fixture_7_score) { fixture_7.create_score(home: 4, away: 0) }
 
       before :each do
-        team_hash.each { |k,v| Team.create(id: v, name: k.to_s) }
-        Team.all.each { |team| GbPoint.create(team_id: team.id, value: 0) }
+        team_hash.each { |k, v| Team.create(id: v, name: k.to_s) }
+        Team.all.each { |team| SweepPoint.create(team_id: team.id, value: 8) }
       end
 
-      context 'when goal buster round is week 1' do
-        let!(:round) { GbRound.create(starting_week_id: 1) }
-        let(:expected_hash) { {1 => 0, 2 => 7, 3 => 9, 4 => 1, 5 => 3, 6 => 11} }
-        let(:expected_minute_hash) { {1 => nil, 2 => 24, 3 => 34, 4 => 41, 5 => 51, 6 => 63} }
+      let(:expected_hash) { {1 => 6, 2 => 8, 3 => 7, 4 => 6, 5 => 5, 6 => 5, 7 => 5, 8 => 5} }
 
-        it 'creates the expected points' do
-          RakeTaskResources::MarkGbBets.perform
+      it 'creates the expected points' do
+        RakeTaskResources::MarkSweepBets.perform
 
-          expected_hash.each do |k, v|
-            expect(GbPoint.find_by_team_id(k).value).to eq(v)
-          end
-        end
-
-        it 'creates the expected minutes' do
-          RakeTaskResources::MarkGbBets.perform
-
-          expected_minute_hash.each do |k, v|
-            #expect(GbPoint.find_by_team_id(k).minute).to eq(v)
-            expect(GbPoint.find_by_team_id(k).minute).to eq(nil)
-          end
+        expected_hash.each do |k, v|
+          expect(SweepPoint.find_by_team_id(k).value).to eq(v)
         end
       end
-
-      context 'when goal buster round is week 2' do
-        let!(:round) { GbRound.create(starting_week_id: 2) }
-        let(:expected_hash) { {1 => 0, 2 => 5, 3 => 10, 4 => 0, 5 => 3, 6 => 11} }
-        let(:expected_minute_hash) { {1 => nil, 2 => 24, 3 => 34, 4 => 41, 5 => 51, 6 => 63} }
-
-        it 'creates the expected points' do
-          RakeTaskResources::MarkGbBets.perform
-
-          expected_hash.each do |k, v|
-            expect(GbPoint.find_by_team_id(k).value).to eq(v)
-          end
-        end
-
-        it 'creates the expected minutes' do
-          RakeTaskResources::MarkGbBets.perform
-
-          expected_minute_hash.each do |k, v|
-            #expect(GbPoint.find_by_team_id(k).minute).to eq(v)
-            expect(GbPoint.find_by_team_id(k).minute).to eq(nil)
-          end
-        end
-      end
-
     end
   end
 end
