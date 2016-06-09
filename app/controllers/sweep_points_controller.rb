02b786113d42584d21e0f8dde0690bf37a90ee54
silversurfer <mathermark@hotmail.com>
2016-06-09 22:23:17 +0100

    Added paid symbol for 5 Alive users

--- app/controllers/sweep_points_controller.rb
+++ app/controllers/sweep_points_controller.rb
@@ -4,6 +4,7 @@ class SweepPointsController < ApplicationController
   def index
     @points = SweepPoint.sorted - [Team.find_by_name('TBA').sweep_point]
     @current_user = current_user
+    @game = Game.find(5)
   end
 
 end
