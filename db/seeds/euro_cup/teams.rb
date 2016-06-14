67fc59b90b60440e9e61713697f075a9c9ff130b
silversurfer <mathermark@hotmail.com>
2016-06-14 23:13:23 +0100

    Enabling auto syncing of fixtures and scores from data feed

--- db/seeds/euro_cup/teams.rb
+++ db/seeds/euro_cup/teams.rb
@@ -1,28 +1,28 @@
 Team.delete_all
 
-Team.find_or_create_by_id(id: 1,  abbreviation: 'alb', name: 'Albania')       #A3
-Team.find_or_create_by_id(id: 2,  abbreviation: 'aut', name: 'Austria')       #F3
-Team.find_or_create_by_id(id: 3,  abbreviation: 'bel', name: 'Belgium')       #E1
-Team.find_or_create_by_id(id: 4,  abbreviation: 'cro', name: 'Croatia')       #D4
-Team.find_or_create_by_id(id: 5,  abbreviation: 'cze', name: 'Czech Rep.')    #D2
-Team.find_or_create_by_id(id: 6,  abbreviation: 'eng', name: 'England')       #B1
-Team.find_or_create_by_id(id: 7,  abbreviation: 'fra', name: 'France')        #A1
-Team.find_or_create_by_id(id: 8,  abbreviation: 'ger', name: 'Germany')       #E1
-Team.find_or_create_by_id(id: 9,  abbreviation: 'hun', name: 'Hungary')       #F4
-Team.find_or_create_by_id(id: 10, abbreviation: 'ice', name: 'Iceland')       #F2
-Team.find_or_create_by_id(id: 11, abbreviation: 'ita', name: 'Italy')         #E2
-Team.find_or_create_by_id(id: 12, abbreviation: 'nir', name: 'N. Ireland')    #C4
-Team.find_or_create_by_id(id: 13, abbreviation: 'pol', name: 'Poland')        #C3
-Team.find_or_create_by_id(id: 14, abbreviation: 'por', name: 'Portugal')      #F1
-Team.find_or_create_by_id(id: 15, abbreviation: 'rir', name: 'Rep. Ireland')  #E3
-Team.find_or_create_by_id(id: 16, abbreviation: 'rom', name: 'Romania')       #A2
-Team.find_or_create_by_id(id: 17, abbreviation: 'rus', name: 'Russia')        #B2
-Team.find_or_create_by_id(id: 18, abbreviation: 'svk', name: 'Slovakia')      #B4
-Team.find_or_create_by_id(id: 19, abbreviation: 'spa', name: 'Spain')         #D1
-Team.find_or_create_by_id(id: 20, abbreviation: 'swe', name: 'Sweden')        #E4
-Team.find_or_create_by_id(id: 21, abbreviation: 'swi', name: 'Switzerland')   #A4
-Team.find_or_create_by_id(id: 22, abbreviation: 'tur', name: 'Turkey')        #D3
-Team.find_or_create_by_id(id: 23, abbreviation: 'ukr', name: 'Ukraine')       #C2
-Team.find_or_create_by_id(id: 24, abbreviation: 'wal', name: 'Wales')         #B3
+Team.find_or_create_by_id(id: 1,  external_id: 1065, abbreviation: 'alb', name: 'Albania')       #A3
+Team.find_or_create_by_id(id: 2,  external_id: 816,  abbreviation: 'aut', name: 'Austria')       #F3
+Team.find_or_create_by_id(id: 3,  external_id: 805,  abbreviation: 'bel', name: 'Belgium')       #E1
+Team.find_or_create_by_id(id: 4,  external_id: 799,  abbreviation: 'cro', name: 'Croatia')       #D4
+Team.find_or_create_by_id(id: 5,  external_id: 798,  abbreviation: 'cze', name: 'Czech Rep.')    #D2
+Team.find_or_create_by_id(id: 6,  external_id: 770,  abbreviation: 'eng', name: 'England')       #B1
+Team.find_or_create_by_id(id: 7,  external_id: 773,  abbreviation: 'fra', name: 'France')        #A1
+Team.find_or_create_by_id(id: 8,  external_id: 759,  abbreviation: 'ger', name: 'Germany')       #E1
+Team.find_or_create_by_id(id: 9,  external_id: 827,  abbreviation: 'hun', name: 'Hungary')       #F4
+Team.find_or_create_by_id(id: 10, external_id: 1066, abbreviation: 'ice', name: 'Iceland')       #F2
+Team.find_or_create_by_id(id: 11, external_id: 784,  abbreviation: 'ita', name: 'Italy')         #E2
+Team.find_or_create_by_id(id: 12, external_id: 829,  abbreviation: 'nir', name: 'N. Ireland')    #C4
+Team.find_or_create_by_id(id: 13, external_id: 794,  abbreviation: 'pol', name: 'Poland')        #C3
+Team.find_or_create_by_id(id: 14, external_id: 765,  abbreviation: 'por', name: 'Portugal')      #F1
+Team.find_or_create_by_id(id: 15, external_id: 806,  abbreviation: 'rir', name: 'Rep. Ireland')  #E3
+Team.find_or_create_by_id(id: 16, external_id: 811,  abbreviation: 'rom', name: 'Romania')       #A2
+Team.find_or_create_by_id(id: 17, external_id: 808,  abbreviation: 'rus', name: 'Russia')        #B2
+Team.find_or_create_by_id(id: 18, external_id: 768,  abbreviation: 'svk', name: 'Slovakia')      #B4
+Team.find_or_create_by_id(id: 19, external_id: 760,  abbreviation: 'spa', name: 'Spain')         #D1
+Team.find_or_create_by_id(id: 20, external_id: 792,  abbreviation: 'swe', name: 'Sweden')        #E4
+Team.find_or_create_by_id(id: 21, external_id: 788,  abbreviation: 'swi', name: 'Switzerland')   #A4
+Team.find_or_create_by_id(id: 22, external_id: 803,  abbreviation: 'tur', name: 'Turkey')        #D3
+Team.find_or_create_by_id(id: 23, external_id: 790,  abbreviation: 'ukr', name: 'Ukraine')       #C2
+Team.find_or_create_by_id(id: 24, external_id: 833,  abbreviation: 'wal', name: 'Wales')         #B3
 
 Team.find_or_create_by_id(id: 25, abbreviation: 'tba', name: 'TBA')
