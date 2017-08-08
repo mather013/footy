eb7b2096b0fed0ceffcd569d9acb7f3c5d42f164
silversurfer <mathermark@hotmail.com>
2017-08-08 21:11:21 +0100

    Added ability to manually add player's goal for a fixture through API

--- app/controllers/api/fixtures_controller.rb
+++ app/controllers/api/fixtures_controller.rb
@@ -4,7 +4,7 @@ module Api
     def add_score
       result = false
       if params_valid(params) && authorised_user(params['username'])
-        fixture = Fixture.find_by_name(params['name'].upcase)
+        fixture = Fixture.find_by(name: params['name'].upcase)
         home_goals = params['score'].split("-").first.to_i
         away_goals = params['score'].split("-").last.to_i
         record_score(fixture, home_goals, away_goals)
@@ -14,10 +14,27 @@ module Api
       render status: 200, json: params.merge(result: result).to_json
     end
 
+    def add_goal
+      result = false
+      if params_valid(params) && authorised_user(params['username'])
+        fixture = Fixture.find_by(name: params['name'].upcase)
+        player = Player.find_by(reference: params['player_ref'].upcase)
+
+        if player.present? && fixture.present?
+          fixture.update_attributes(status: Fixture::Status::IN_PLAY) unless fixture.status == Fixture::Status::FINISHED
+          fixture.score.create(home: 0, away: 0) if fixture.score.nil?
+          fixture.score.goals.create(player_id: player.id)
+          result = fixture.score.goals.create(player_id: player.id)
+        end
+      end
+
+      render status: 200, json: params.merge(result: result).to_json
+    end
+
     private
 
     def params_valid(params)
-      params['name'].present? && params['score'].present? && params['username'].present?
+      params['name'].present? && params['username'].present? && (params['score'].present? || params['player_ref'].present? )
     end
 
     def authorised_user(username)
