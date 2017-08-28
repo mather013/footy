e6fced28008ff28ffc349a1906f681795dbfc39a
silversurfer <mathermark@hotmail.com>
2017-08-28 21:50:55 +0100

    Added scope to 5 Alive bets to allow selecting bets for specific round

--- app/controllers/fat_bets_controller.rb
+++ app/controllers/fat_bets_controller.rb
@@ -2,7 +2,7 @@ class FatBetsController < ApplicationController
   before_action :require_login
 
   def index
-    @bets = current_user.fat_bets.sorted #by fat_round_id
+    @bets = current_user.fat_bets.for_round_sorted(params['fat_round_id'])
     @round = @bets.first.fat_round
   end
 
--- app/models/fat_bet.rb
+++ app/models/fat_bet.rb
@@ -4,7 +4,7 @@ class FatBet < ActiveRecord::Base
   belongs_to :fat_round
   belongs_to :player
 
-  scope :sorted, -> { order('id asc') }
+  scope :for_round_sorted, -> (round) { where(fat_round_id: round).order('id asc') }
 
   def fixture
     @fixture ||= fat_round.week.fixtures.where('home_team_id=? or away_team_id=?', player.team_id, player.team_id).first
