a021036a06ba643be35a393841e93723401667c0
silversurfer <mathermark@hotmail.com>
2017-11-28 17:52:47 +0000

    UI Improvements when adding goals scorers

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -7,24 +7,24 @@ class GoalsController < ApplicationController
 
   def index
     @fixture = Fixture.find(params['fixture_id'])
-    @week = @fixture.week
-
     @players = @fixture.home_team.players.position_order + @fixture.away_team.players.position_order
     @goals = @fixture.score.goals if @fixture.score.present?
   end
 
   def update
-    fixture = Fixture.find(params['fixture_id'])
+    @fixture = Fixture.find(params['fixture_id'])
+
     player_ids = params['player_ids'].split(',').map(&:to_i)
 
-    if fixture.present? && player_ids.present?
-      fixture.update_attributes(status: Fixture::Status::IN_PLAY) unless fixture.status == Fixture::Status::FINISHED
-      fixture.create_score(home: 0, away: 0) if fixture.score.nil?
+    if @fixture.present? && player_ids.present?
+      @fixture.update_attributes(status: Fixture::Status::IN_PLAY) unless @fixture.status == Fixture::Status::FINISHED
+      @fixture.create_score(home: 0, away: 0) if @fixture.score.nil?
 
       player_ids.each do |player_id|
-        fixture.score.goals.create(player_id: player_id) unless fixture.score.goals.map(&:player_id).include?(player_id)
+        @fixture.score.goals.create(player_id: player_id) unless @fixture.score.goals.map(&:player_id).include?(player_id)
       end
     end
+    @scorers = @fixture.score.goals.map(&:player).uniq
   end
 
 end
