f4f2e926239acbdb046d892176cbd4cee23abedb
silversurfer <mathermark@hotmail.com>
2018-01-03 21:55:45 +0000

    Updated weeks, fixtures and teams for world cup 2018

--- db/seeds/world_cup/weeks.rb
+++ db/seeds/world_cup/weeks.rb
@@ -1,36 +1,9 @@
 Week.delete_all
 
-Week.find_or_create_by_id(:id => 1,
-                          :description => 'Group Stage 1',
-                          :close_date => DateTime.parse('12-06-2014 20:00:00'),
-                          :complete => true)
-
-Week.find_or_create_by_id(:id => 2,
-                          :description => 'Group Stage 2',
-                          :close_date => DateTime.parse('17-06-2014 19:00:00'),
-                          :complete => true)
-
-Week.find_or_create_by_id(:id => 3,
-                          :description => 'Group Stage 3',
-                          :close_date => DateTime.parse('23-06-2014 16:00:00'),
-                          :complete => true)
-
-Week.find_or_create_by_id(:id => 4,
-                          :description => 'Round of 16',
-                          :close_date => DateTime.parse('28-06-2014 16:00:00'),
-                          :complete => true)
-
- Week.find_or_create_by_id(:id => 5,
-                           :description => 'Quarter-final',
-                           :close_date => DateTime.parse('04-07-2014 16:00:00'),
-                           :complete => true)
- 
- Week.find_or_create_by_id(:id => 6,
-                           :description => 'Semi-final',
-                           :close_date => DateTime.parse('08-07-2014 20:00:00'),
-                           :complete => true)
- 
- Week.find_or_create_by_id(:id => 7,
-                           :description => 'Final',
-                           :close_date => DateTime.parse('12-07-2014 20:00:00'),
-                           :complete => nil)
+Week.create(id: 1, description: 'Group Stage 1', close_date: DateTime.parse('14-06-2018 15:00:00'))
+Week.create(id: 2, description: 'Group Stage 2', close_date: DateTime.parse('19-06-2018 18:00:00'))
+Week.create(id: 3, description: 'Group Stage 3', close_date: DateTime.parse('25-06-2018 14:00:00'))
+Week.create(id: 4, description: 'Round of 16',   close_date: DateTime.parse('30-06-2018 14:00:00'))
+Week.create(id: 5, description: 'Quarter-final', close_date: DateTime.parse('06-07-2018 14:00:00'))
+Week.create(id: 6, description: 'Semi-final',    close_date: DateTime.parse('10-07-2018 19:00:00'))
+Week.create(id: 7, description: 'Final',         close_date: DateTime.parse('14-07-2018 14:00:00'))
