6b0ed2969d7affd051ba8f3e6b5f14281dde01c9
silversurfer <mathermark@hotmail.com>
2018-04-22 20:56:44 +0100

    World Cup 2018 data (teams, weeks and fixtures)

---
+++ db/seeds/world_cup/groups.rb
@@ -0,0 +1,51 @@
+Group.delete_all
+GroupTeam.delete_all
+
+Group.create(id: 1, name: 'Group A')
+Group.create(id: 2, name: 'Group B')
+Group.create(id: 3, name: 'Group C')
+Group.create(id: 4, name: 'Group D')
+Group.create(id: 5, name: 'Group E')
+Group.create(id: 6, name: 'Group F')
+Group.create(id: 7, name: 'Group G')
+Group.create(id: 8, name: 'Group H')
+
+# GROUP A
+[24, 25, 9, 32].each do |team_id|
+  GroupTeam.create(group_id: 1, team_id: team_id)
+end
+
+# GROUP B
+[18, 14, 23, 28].each do |team_id|
+  GroupTeam.create(group_id: 2, team_id: team_id)
+end
+
+# GROUP C
+[11, 2, 1, 13].each do |team_id|
+  GroupTeam.create(group_id: 3, team_id: team_id)
+end
+
+# GROUP D
+[21, 8, 7, 19].each do |team_id|
+  GroupTeam.create(group_id: 4, team_id: team_id)
+end
+
+# GROUP E
+[6, 27, 12, 17].each do |team_id|
+  GroupTeam.create(group_id: 5, team_id: team_id)
+end
+
+# GROUP F
+[4, 30, 29, 16].each do |team_id|
+  GroupTeam.create(group_id: 6, team_id: team_id)
+end
+
+# GROUP G
+[3, 20, 31, 10].each do |team_id|
+  GroupTeam.create(group_id: 7, team_id: team_id)
+end
+
+# GROUP H
+[5, 15, 22, 26].each do |team_id|
+  GroupTeam.create(group_id: 8, team_id: team_id)
+end
