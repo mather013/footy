7b6a9db45df7cf1d5fc697fe3e9e27ed6392c189
silversurfer <mathermark@hotmail.com>
2018-06-01 21:24:47 +0100

    Toggling analytics off

--- lib/services/analytics_service.rb
+++ lib/services/analytics_service.rb
@@ -3,7 +3,7 @@ module Services
 
     def self.publish event, params
       params.merge!({ environment: Rails.env })
-      Keen.publish(event.to_sym, params) if Rails.env.production?
+      Keen.publish(event.to_sym, params) if TOGGLES_CONFIG['analytics'] && Rails.env.production?
     end
 
   end
