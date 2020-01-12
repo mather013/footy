944658bbbe8413bd0f077a27cc5ca13cdd96298b
silversurfer <mathermark@hotmail.com>
2020-01-12 20:46:57 +0000

    Fix to enable manual running of jobs through controller

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -6,7 +6,12 @@ class JobsController < ApplicationController
   end
 
   def perform
-    class_name = "Jobs::#{params[:job_name].camelize}".constantize
+    if params[:job_name].include?('sync')
+      class_name = "Jobs::Syncing::#{params[:job_name].camelize}".constantize
+    else
+      class_name = "Jobs::#{params[:job_name].camelize}".constantize
+    end
+
     class_name.new.perform
     redirect_to jobs_url
   end
--- app/models/jobs.rb
+++ app/models/jobs.rb
@@ -1,7 +1,7 @@
 module Jobs
   def self.all
     [Jobs::Syncing::SyncFixtures, Jobs::Syncing::SyncScores, Jobs::MarkWeek].collect do |job|
-      job.to_s.gsub('Jobs::', '').underscore
+      job.to_s.gsub('Jobs::', '').gsub('Syncing::','').underscore
     end
 
   end
