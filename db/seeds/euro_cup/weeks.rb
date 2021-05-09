4f73aa42cb000a961a08b646ef27d08cde60d18a
Mark Mather <mark@mather-air.home>
2021-05-09 08:41:45 +0100

    Updating weeks, fixtures and teams for Euro 2021

--- db/seeds/euro_cup/weeks.rb
+++ db/seeds/euro_cup/weeks.rb
@@ -1,9 +1,9 @@
 Week.delete_all
 
-Week.find_or_create_by_id(id: 1, description: 'Group Stage 1', close_date: DateTime.parse('10-06-2016 19:00:00'))
-Week.find_or_create_by_id(id: 2, description: 'Group Stage 2', close_date: DateTime.parse('15-06-2016 13:00:00'))
-Week.find_or_create_by_id(id: 3, description: 'Group Stage 3', close_date: DateTime.parse('19-06-2016 19:00:00'))
-Week.find_or_create_by_id(id: 4, description: 'Round of 16',   close_date: DateTime.parse('25-06-2016 13:00:00'))
-Week.find_or_create_by_id(id: 5, description: 'Quarter-final', close_date: DateTime.parse('30-06-2016 19:00:00'))
-Week.find_or_create_by_id(id: 6, description: 'Semi-final',    close_date: DateTime.parse('06-07-2016 19:00:00'))
-Week.find_or_create_by_id(id: 7, description: 'Final',         close_date: DateTime.parse('10-07-2016 19:00:00'))
+Week.find_or_create_by_id(id: 1, description: 'Group Stage 1', close_date: DateTime.parse('11-06-2021 19:00:00'))
+Week.find_or_create_by_id(id: 2, description: 'Group Stage 2', close_date: DateTime.parse('16-06-2021 16:00:00'))
+Week.find_or_create_by_id(id: 3, description: 'Group Stage 3', close_date: DateTime.parse('20-06-2021 16:00:00'))
+Week.find_or_create_by_id(id: 4, description: 'Round of 16',   close_date: DateTime.parse('26-06-2021 16:00:00'))
+Week.find_or_create_by_id(id: 5, description: 'Quarter-final', close_date: DateTime.parse('02-07-2021 16:00:00'))
+Week.find_or_create_by_id(id: 6, description: 'Semi-final',    close_date: DateTime.parse('06-07-2021 19:00:00'))
+Week.find_or_create_by_id(id: 7, description: 'Final',         close_date: DateTime.parse('11-07-2021 19:00:00'))
